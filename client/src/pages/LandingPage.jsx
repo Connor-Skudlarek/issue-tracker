1f1b773091a488eb66af2a15ea08ef33a8208174
Connor Skudlarek <connor.skudlarek@gmail.com>
2023-07-13 16:52:14 -0700

    Add call-to-action & styles to landing page, fixes #2

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -4,7 +4,21 @@ import LoginForm from "../components/LoginForm";
 function LandingPage() {
   return (
     <div className="LandingPage">
-      <div className="form-container">
+      <div className="LandingPage-app-title">
+        <h1>Issue Tracker</h1>
+      </div>
+      <div className="LandingPage-call-to-action">
+        <p className="LandingPage-statement">Start tracking your project issues today!</p>
+        <p className="LandingPage-try-catch-cleverness">
+          try {"{"} <br></br>
+          &nbsp;&nbsp;troublesomeCode()<br></br>
+          {"}"} catch (error) {"{"}
+          <br></br>
+          &nbsp;&nbsp;trackIssue(error)<br></br>
+          {"}"}
+        </p>
+      </div>
+      <div className="LandingPage-form-container">
         <LoginForm />
       </div>
     </div>
