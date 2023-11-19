e46a6775aab6405b078a09d22f50e53d3d0dd7ec
Connor Skudlarek <connor.skudlarek@gmail.com>
2023-11-18 18:31:14 -0800

    Removed unused lines

--- app/lib/login-actions.tsx
+++ app/lib/login-actions.tsx
@@ -1,8 +1,4 @@
 "use server";
-// import { z } from "zod";
-// import { sql } from "@vercel/postgres";
-// import { revalidatePath } from "next/cache";
-// import { redirect } from "next/navigation";
 import { signIn } from "@/auth";
 
 export async function authenticate(
