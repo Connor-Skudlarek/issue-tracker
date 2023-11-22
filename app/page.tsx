562629384dbf905290ea5e3c74b1a487f626cf32
Connor-Skudlarek <connor.skudlarek@gmail.com>
2023-11-21 19:34:41 -0800

    Add basic mostly unstylized landing page, #8

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,49 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link className="mb-2 flex h-20 items-center rounded-md bg-blue-500 p-4 md:h-30" href="/login">
-        <div className="mb-2 text-white text-2xl text-center">Get started by logging in!</div></Link>
+    <main className="h-100 flex flex-row items-center justify-between overflow-auto bg-my-grey p-0">
+      <div id="landing-image" className="h-screen w-6/12 bg-my-navy">
+        <Image
+          src="/images/landing-pic.jpg"
+          className="max-h-screen w-fit"
+          width={1024}
+          height={1024}
+          alt="A picture of 6 figures working on machines"
+        />
+      </div>
+
+      <div
+        id="landing-container"
+        className="flex h-screen w-5/12 flex-col justify-between"
+      >
+        <div id="call-to-action">
+          <h1 className="text-my-navy-blue m-6 p-12 pt-24 text-center text-4xl">
+            Wafer Wizards
+          </h1>
+
+          <p>
+            Have a lot of people working several positions, all on the same
+            issue?
+          </p>
+        </div>
+
+        <div id="screenshot"></div>
+
+        <Link
+          className="md:h-30 w-50 m-12 items-center rounded-md bg-my-mint/[0.98] p-4"
+          href="/login"
+        >
+          <div className="mb-2 text-center text-2xl text-my-navy">
+            Get started by logging in!
+          </div>
+        </Link>
+
+        <div id="copyright" className="pb-4 pr-12 text-right">
+          © 2023 Connor Skudlarek — Powered by Vercel
+        </div>
+      </div>
     </main>
-  )
+  );
 }
