e7dae56be96e785c2d7cd1c3755982d1cd7b75b9
Connor-Skudlarek <connor.skudlarek@gmail.com>
2023-11-22 19:31:17 -0800

    Tune landing page, add text, logo, style, fixes #8

--- app/page.tsx
+++ app/page.tsx
@@ -3,41 +3,50 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="h-100 flex flex-row items-center justify-between overflow-auto bg-my-grey p-0">
-      <div id="landing-image" className="h-screen w-6/12 bg-my-navy">
+    <main className="h-100 flex flex-col items-center justify-between overflow-auto bg-my-grey p-0 md:flex-row">
+      <div
+        id="landing-image"
+        className="justify-center bg-my-navy lg:h-screen lg:w-7/12 lg:bg-my-navy"
+      >
         <Image
           src="/images/landing-pic.jpg"
-          className="max-h-screen w-fit"
+          className="max-h-screen justify-center px-6 py-2"
           width={1024}
           height={1024}
-          alt="A picture of 6 figures working on machines"
+          alt="8 people working on machines"
         />
       </div>
 
       <div
         id="landing-container"
-        className="flex h-screen w-5/12 flex-col justify-between"
+        className="flex h-screen flex-col justify-between lg:w-5/12"
       >
         <div id="call-to-action">
-          <h1 className="text-my-navy-blue m-6 p-12 pt-24 text-center text-4xl">
+          <h1 className="align-center m-12 m-6 rounded-lg bg-my-navy p-4 text-center text-4xl font-bold text-my-mint shadow">
             Wafer Wizards
           </h1>
-
-          <p>
-            Have a lot of people working several positions, all on the same
-            issue?
+          <p className="box font-300 m-8 rounded-xl bg-my-mint/[0.5] p-4 text-lg shadow-my-mint">
+            Having a hard time tracking your projects? Unsure what is in
+            maintenance and what is in production? Who is working on the
+            problem? When will it be back to normal?<br></br>
+            <br></br>We got some magic for that!
           </p>
         </div>
 
-        <div id="screenshot"></div>
+        <div id="screenshot" className="flex justify-center object-contain">
+          <Image
+            src="/images/logo.png"
+            width={128}
+            height={128}
+            alt="Wafer Wizards Icon"
+          ></Image>
+        </div>
 
         <Link
-          className="md:h-30 w-50 m-12 items-center rounded-md bg-my-mint/[0.98] p-4"
+          className="md:h-30 w-50 mx-8 mt-4 rounded-md bg-my-mint/[0.98] object-contain p-0 p-4 text-center text-2xl text-my-navy hover:bg-my-navy hover:text-my-mint"
           href="/login"
         >
-          <div className="mb-2 text-center text-2xl text-my-navy">
-            Get started by logging in!
-          </div>
+          Get started by logging in!
         </Link>
 
         <div id="copyright" className="pb-4 pr-12 text-right">
