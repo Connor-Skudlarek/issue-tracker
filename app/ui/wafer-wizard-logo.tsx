6f7d5c0a1bc08fb2a8d23f715a3a70aee0392fe3
Connor-Skudlarek <connor.skudlarek@gmail.com>
2023-11-26 16:48:07 -0800

    Schange text to update on md-screen
    Found an extra bug. With sm:text-[44px] it would start at a sm-screen.
    Updated it to start on md-screen.

--- app/ui/wafer-wizard-logo.tsx
+++ app/ui/wafer-wizard-logo.tsx
@@ -7,7 +7,7 @@ export default function WaferWizardsLogo() {
       className={`${lusitana.className} flex h-fit w-fit flex-row items-center bg-my-mint leading-none text-my-navy/100`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[32px] sm:text-[44px]">Wafer Wizards</p>
+      <p className="text-[32px] md:text-[44px]">Wafer Wizards</p>
     </div>
   );
 }
