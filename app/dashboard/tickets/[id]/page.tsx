9e378b0a4ac31bf413214fc0537ec2a6a972eef3
Connor Skudlarek <connor.skudlarek@gmail.com>
2023-12-03 21:06:33 -0800

    Add a id to page

--- app/dashboard/tickets/[id]/page.tsx
+++ app/dashboard/tickets/[id]/page.tsx
@@ -1,7 +1,12 @@
+"use client"
+import { usePathname } from "next/navigation";
+
 export default function page() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const id = usePathname().split("/").pop()
   return (
     <div>
-      This is where individual tickets can be viewed. Only devs and admins will
+      This is where individual tickets can be viewed. This one is ID: {id}. Only devs and admins will
       see this in the future.
     </div>
   );
