3eca6bd109434e14a6174e5257fe6385292c482f
Connor-Skudlarek <connor.skudlarek@gmail.com>
2023-12-03 18:53:57 -0800

    Add tickets page, #4

---
+++ app/dashboard/new-ticket/page.tsx
@@ -0,0 +1,10 @@
+export default function page() {
+  return (
+    <div>
+      <p>
+        This page is where new tickets can be submitted. Devs, admins, and users
+        will be able to do this.
+      </p>
+    </div>
+  );
+}
