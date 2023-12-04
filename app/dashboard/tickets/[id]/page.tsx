cebd312a35b6bd76a015e86bdf40863c018cdf68
Connor-Skudlarek <connor.skudlarek@gmail.com>
2023-12-03 18:54:17 -0800

    Add individual ticket page, #4

---
+++ app/dashboard/tickets/[id]/page.tsx
@@ -0,0 +1,8 @@
+export default function page() {
+  return (
+    <div>
+      This is where individual tickets can be viewed. Only devs and admins will
+      see this in the future.
+    </div>
+  );
+}
