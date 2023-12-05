7ab59d2adcb2d4df8a213e382b02053df94ab0ca
Connor Skudlarek <connor.skudlarek@gmail.com>
2023-12-05 06:35:50 -0800

    Start filling in placeholder data

---
+++ app/lib/placeholder-data.js
@@ -0,0 +1,41 @@
+const ww_users = [{
+    id: "420124b2-4001-4271-9855-sec4b6a6442a",
+    name: "Nick Fury",
+    email: "OneEyeOpen@shield.org",
+    password: "patches",
+    role: "admin"
+},
+{
+    id: "d6e15727-9fe1-4961-8c5b-ea44a9bd81aa",
+    name: "Tony Stark",
+    email: "thegenius@starkindustries.com",
+    password: "IAMIRONMAN",
+    role: "developer"
+},
+{
+    id: "CC27C14A-0ACF-4F4A-A6C9-D45682C144B9",
+    name: "Peter Parker",
+    email: "peter.parker@imadeanemail.com",
+    password: "th3NextAvenger!",
+    role: "developer"
+},
+{
+    id: "126eed9c-c90c-4ef6-a4a8-fcf7408d3c66",
+    name: "Steve Rogers",
+    email: "OutOfTime@avengers.org",
+    password: "Peggy1921",
+    role: "user"
+},
+{
+    id: "13D07535-C59E-4157-A011-F8D2EF4E0CBB",
+    name: "Thor",
+    email: "Thor@yahoo.com",
+    password: "Thor",
+    role: "user"
+}]
+
+const ww_tickets = {}
+
+const ww_tasks = {}
+
+const ww_comments = {}
