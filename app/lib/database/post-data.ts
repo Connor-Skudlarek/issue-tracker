2fc21cb4f4892993fa76dbf21a36a8ad5caecf83
Connor-Skudlarek <connor.skudlarek@gmail.com>
2023-12-10 18:58:10 -0800

    Add bare POST functions, #4

---
+++ app/lib/database/post-data.ts
@@ -0,0 +1,18 @@
+import { sql } from "@vercel/postgres";
+import { newUser, newTicket, newTask, newComment } from "../definitions";
+
+export async function createNewUser() {
+  // New user logic here
+}
+
+export async function createNewTicket() {
+  // New ticket logic here
+}
+
+export async function createNewTask() {
+  // New task logic here
+}
+
+export async function createNewComment() {
+  // New comment logic here
+}
