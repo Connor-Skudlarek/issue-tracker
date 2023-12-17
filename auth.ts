5cd3c630077eeaeecd9501f3d2625fb443b86725
Connor Skudlarek <connor.skudlarek@gmail.com>
2023-12-16 19:11:17 -0800

    Change users table to correct name, #4

--- auth.ts
+++ auth.ts
@@ -8,7 +8,7 @@ import { sql } from "@vercel/postgres";
 
 async function getUser(email: string): Promise<User | undefined> {
   try {
-    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    const user = await sql<User>`SELECT * FROM ww_users WHERE email=${email}`;
     return user.rows[0];
   } catch (error) {
     console.error("Failed to fetch user:", error);
