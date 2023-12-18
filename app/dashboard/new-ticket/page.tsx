6ff3763ec777a2d9895067c8d20b64938db92d3c
Connor-Skudlarek <connor.skudlarek@gmail.com>
2023-12-17 17:14:05 -0800

    Change priority from a number to a radio, #4

--- app/dashboard/new-ticket/page.tsx
+++ app/dashboard/new-ticket/page.tsx
@@ -1,17 +1,44 @@
-
 import { usePathname } from "next/navigation";
 import { FormEvent } from "react";
 import { Button } from "../../ui/button";
 import { createNewTicket } from "../../lib/tickets/new-ticket";
 export default function page() {
-
   return (
     <div>
       This is where individual tickets can be submitted.
       <form action={createNewTicket} className="flex flex-col">
         <label htmlFor="priority">
           Priority Level:{" "}
-          <input id="priority" name="priority" type="number"></input>
+          <fieldset>
+            <legend>Priority level: </legend>
+            <div id="priority">
+              <input type="radio" value="1" id="P1" />
+              <label htmlFor="P1">
+                1—Critical {"(needs immediate attention and problem is: "}
+                <span className="font-bold">UNKNOWN{")"}</span>
+              </label>
+              <input type="radio" value="2" id="P2" />
+              <label htmlFor="P2">
+                2—High {"(needs immediate attention and problem is: "}
+                <span className="font-bold">KNOWN{")"}</span>
+              </label>
+
+              <input type="radio" value="3" id="P3" />
+              <label htmlFor="P3">
+                3—Moderate {"(needs resources as available—time sensitive)"}
+              </label>
+
+              <input type="radio" value="4" id="P4" />
+              <label htmlFor="P4">
+                4—Low {"(needs resources as available—time irrelevant)"}
+              </label>
+              <input type="radio" value="5" id="P5" />
+              <label htmlFor="P5">
+                5—Unclear/Exploratory{" "}
+                {"(issue not explored enough to prioritize)"}
+              </label>
+            </div>
+          </fieldset>
         </label>
         <label htmlFor="description">
           Description: <input id="description" name="description"></input>
